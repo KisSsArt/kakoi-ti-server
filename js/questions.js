7dff7abdb38f5d676c878f8a5d1c28236a1e9989
artem <59938902+KisSsArt@users.noreply.github.com>
2023-11-16 17:18:07 +0300

    Update questions.js
    add questions

--- js/questions.js
+++ js/questions.js
@@ -2,10 +2,114 @@ let current_question = 0;
 let questions = [
     // 0 //
     {
-        question: "question 1"
+        question: "question 1",
+        answers: ["ответ 1", "ответ 2"]
     },
     // 1 //
     {
-        question: "question 2"
+        question: "question 2",
+        answers: ["ответ 1", "ответ 2"]
+    },
+    // 2 //
+    {
+        question: "question 3",
+        answers: ["ответ 1", "ответ 2"]
+    },
+    // 3 //
+    {
+        question: "question 4",
+        answers: ["ответ 1", "ответ 2"]
+    },
+    // 4 //
+    {
+        question: "question 5",
+        answers: ["ответ 1", "ответ 2"]
+    },
+    // 5 //
+    {
+        question: "question 6",
+        answers: ["ответ 1", "ответ 2"]
+    },
+    // 6 //
+    {
+        question: "question 7",
+        answers: ["ответ 1", "ответ 2"]
+    },
+    // 7 //
+    {
+        question: "question 8",
+        answers: ["ответ 1", "ответ 2"]
+    },
+    // 8 //
+    {
+        question: "question 9",
+        answers: ["ответ 1", "ответ 2"]
+    },
+    // 9 //
+    {
+        question: "question 10",
+        answers: ["ответ 1", "ответ 2"]
+    }
+]
+let results = [
+    // 0 //
+    {
+        result: "вы чайник spacex 1",
+        text: "поздравляю",
+        path: ""
+    },
+    // 1 //
+    {
+        result: "вы чайник spacex 2",
+        text: "поздравляю",
+        path: ""
+    },
+    // 2 //
+    {
+        result: "вы чайник spacex 3",
+        text: "поздравляю",
+        path: ""
+    },
+    // 3 //
+    {
+        result: "вы чайник spacex 4",
+        text: "поздравляю",
+        path: ""
+    },
+    // 4 //
+    {
+        result: "вы чайник spacex 5",
+        text: "поздравляю",
+        path: ""
+    },
+    // 5 //
+    {
+        result: "вы чайник spacex 6",
+        text: "поздравляю",
+        path: ""
+    },
+    // 6 //
+    {
+        result: "вы чайник spacex 7",
+        text: "поздравляю",
+        path: ""
+    },
+    // 7 //
+    {
+        result: "вы чайник spacex 8",
+        text: "поздравляю",
+        path: ""
+    },
+    // 8 //
+    {
+        result: "вы чайник spacex 9",
+        text: "поздравляю",
+        path: ""
+    },
+    // 9 //
+    {
+        result: "вы чайник spacex 10",
+        text: "поздравляю",
+        path: ""
     }
 ]
