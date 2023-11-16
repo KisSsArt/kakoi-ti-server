ad70ba5983955efb808ab6c7a35a8f0d2079b866
artem <59938902+KisSsArt@users.noreply.github.com>
2023-11-17 01:11:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@ document.addEventListener('DOMContentLoaded', function()
 {
     let option = document.createElement('div');
 
-    option.value = questions[0].question;
-    option.innerText = (questions[0].question).toUpperCase();
+    option.value = questions[current_question].question;
+    option.innerText = questions[current_question].question;
 
     document.body.appendChild(option);
 
-    var button = document.createElement('button');
-    button.textContent = 'Нажми меня!';
-    button.addEventListener('click', function() {
-        alert('Вы нажали на кнопку!');
-        current_question++;
-    });
-    
-    document.body.appendChild(button);
+    for (let i = 0; i < 2; i++)
+    {
+        var answer_button = document.createElement("button");
+        answer_button.textContent = questions[current_question].answers[i];
+        answer_button.addEventListener('click', function() {
+            alert('Вы нажали на кнопку!');
+            if (current_question < questions.length)
+                current_question++;
+        });
+
+        document.body.appendChild(answer_button);
+    }
 });
