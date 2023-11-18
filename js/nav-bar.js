ab454cec859236dfe2e3fcb3e9facbbfb509cf44
artem <59938902+KisSsArt@users.noreply.github.com>
2023-11-18 18:13:42 +0300

    Update nav-bar.js

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -3,7 +3,7 @@ function githubButton()
     var button = document.createElement("button");
     button.title="git";
     button.ariaHidden = true;
-    button.className = "fa fa-github fa-3x";
+    button.className = "git-button fa fa-github fa-3x";
     button.addEventListener('click', function() { window.open("https://github.com/KisSsArt"); });
     document.getElementById("nav-form").appendChild(button);
 }
